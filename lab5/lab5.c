85572990a05ad9109304f5fe0ed99fcba4388ed3
Doinet <153589737+sarawutnanthawong@users.noreply.github.com>
2025-07-24 15:29:06 +0700

    Add files via upload

---
+++ lab5/lab5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int score = 0;
+    printf("Enter your score: ");
+    scanf("%d", &score);
+    printf("Grade :");
+    if( score >= 80) {
+        printf("A");
+    } else if (score >= 75) {
+        printf("B+");
+    } else if (score >= 70) {
+        printf("B");
+    } else if (score >= 65) {
+        printf("C+");
+    } else if (score >= 60) {
+        printf("C");
+    } else if (score >= 65) {
+        printf("D+");
+    } else if (score >= 50) {
+        printf("D");    
+    } else {
+        printf("F");
+    }
+    return 0; 
+}
