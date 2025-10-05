864ddca76970c348f635371733415607075a5600
Doinet <153589737+sarawutnanthawong@users.noreply.github.com>
2025-10-05 15:00:05 +0700

    Add files via upload

---
+++ lad1/lab1_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+int x = 0 ;
+int y = 0 ;
+int z = 0 ;
+printf("enter ur number :");
+scanf("%d",&x);
+printf("enter ur number :");
+scanf("%d",&y);
+printf("enter ur number :");
+scanf("%d",&z);
+int xyz = 0;
+xyz = x + y;
+xyz = xyz + z;
+printf("%d",xyz);
+
+}
