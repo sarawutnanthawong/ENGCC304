4658048b72627dba7bad4974ea7c7520e13f9ddd
Doinet <153589737+sarawutnanthawong@users.noreply.github.com>
2025-10-05 16:33:22 +0700

    Add files via upload

---
+++ lab3/Lab3_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int W = 0 ;
+    int H = 0 ;
+    printf("enter ur H : ");
+    scanf("%d", &H);
+    printf("enter ur W : ");
+    scanf("%d",&W);
+    int Area = 0 ;
+    Area = 0.5*(H*W) ;
+    printf("Area = %d",Area);
+    return 0 ;
+
+}
